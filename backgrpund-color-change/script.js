8af2b800f7334f946de2c27b24c7d1a36f794ad9
soniyataneja <92579936+soniyataneja@users.noreply.github.com>
2024-04-30 00:01:12 +0530

    Delete backgrpund-color-change directory

--- backgrpund-color-change/script.js
+++
@@ -1,7 +0,0 @@
-const colorInput = document.getElementById('colorInput');
-const changeButton = document.getElementById('changeButton');
-
-changeButton.addEventListener('click', function() {
-  const color = colorInput.value;
-  document.body.style.backgroundColor = color;
-});
